b47dd318f1596c60796871aada7c803ea2fc7b59
Armand_laptop <517226@edu.rocmn.nl>
2019-03-11 23:13:40 +0100

    spelers index toegevoegd

---
+++ Controller/Spelers_controller.php
@@ -0,0 +1,20 @@
+<?php
+    /**
+     *
+     */
+    class Spelers_controller
+    {
+        function __construct() {
+
+        }
+
+        public function index() {
+            $main = 'index_spelers.php';
+            include 'View/default_template.php';
+
+        }
+
+    }
+
+
+ ?>
