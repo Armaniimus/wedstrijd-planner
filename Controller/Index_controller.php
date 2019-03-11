ef5b10fd9c452a0691cef3ed9ec9066973ac4dfa
Armand_laptop <517226@edu.rocmn.nl>
2019-03-11 23:12:15 +0100

    template homepage toegevoegd

---
+++ Controller/Index_controller.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     *
+     */
+    class Index_controller
+    {
+        function __construct() {
+
+        }
+
+        public function home() {
+            $main = 'index.php';
+            include 'View/default_template.php';
+        }
+    }
+
+
+ ?>
---
+++ View/includes/header.php
@@ -0,0 +1,7 @@
+<nav class='main-nav'>
+    <ul>
+        <li><a href = "<?php echo ROOT?>/home">Home</a></li>
+        <li><a href = "<?php echo ROOT?>/tournooi/index">Tournooi planner</a></li>
+        <li><a href = "<?php echo ROOT?>/spelers/index">Spelers bewerken</a></li>
+    </ul>
+</nav>
